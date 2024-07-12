2dfcfb0fe9fd363519fde23047fa5bceea8a0b9a
Danilo Augusto <dandevweb@gmail.com>
2024-07-12 14:24:00 -0300

    feat: showing and updating openings

--- handler/showOpnening.go
+++ handler/showOpnening.go
@@ -3,11 +3,22 @@ package handler
 import (
 	"net/http"
 
+	"github.com/dandevweb/gopportunities/schema"
 	"github.com/gin-gonic/gin"
 )
 
 func ShowOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "GET /api/v1/openings/" + ctx.Param("id"),
-	})
+	id := ctx.Param("id")
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, "id is required")
+		return
+	}
+	opening := schema.Opening{}
+	if err := db.First(&opening, id).Error; err != nil {
+		logger.Errorf("error finding opening: %v", err.Error())
+		sendError(ctx, http.StatusNotFound, err.Error())
+		return
+	}
+
+	sendSuccess(ctx, "Opening found", opening)
 }
--- handler/updateOpening.go
+++ handler/updateOpening.go
@@ -3,11 +3,34 @@ package handler
 import (
 	"net/http"
 
+	"github.com/dandevweb/gopportunities/schema"
 	"github.com/gin-gonic/gin"
 )
 
 func UpdateOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "PUT /api/v1/openings",
-	})
+	id := ctx.Param("id")
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, "id is required")
+		return
+	}
+	opening := schema.Opening{}
+	if err := db.First(&opening, id).Error; err != nil {
+		logger.Errorf("error finding opening: %v", err.Error())
+		sendError(ctx, http.StatusNotFound, err.Error())
+		return
+	}
+
+	if err := ctx.BindJSON(&opening); err != nil {
+		logger.Errorf("error binding opening: %v", err.Error())
+		sendError(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	if err := db.Save(&opening).Error; err != nil {
+		logger.Errorf("error saving opening: %v", err.Error())
+		sendError(ctx, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	sendSuccess(ctx, "Opening updated", opening)
 }
