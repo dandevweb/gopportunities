4c730d1f2d3b3fd2e96162ba2e34f9f3dbf2edb2
Danilo Augusto <dandevweb@gmail.com>
2024-07-11 21:19:40 -0300

    feat: add custom log

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
+	"github.com/dandevweb/gopportunities/config"
 	"github.com/dandevweb/gopportunities/router"
 )
 
+var (
+	logger *config.Logger
+)
+
 func main() {
+	logger = config.GetLogger("main")
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("Error initializing config: %v", err)
+		return
+	}
+
 	router.Initialize()
 }
